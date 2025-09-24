eca8b2fb96b63e13905cc2df4335701db4408d44
André Sousa <andre_sbatista@outlook.com>
2025-09-24 23:34:50 +0000

    Commit do desafio nivel aventureiro: foi adicionado as variaveis densidade_carta_um e per_capta_carta_um fazendo a conversão explícita e também foi adicionado ambas as saidas no printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,12 +9,12 @@ int main()
     //VARIAVEIS CARTA UM
     char estado_carta_um,codigo_carta_um[4],nome_carta_um[100];
     int populacao_carta_um,pontos_turisticos_carta_um;
-    float area_carta_um,pib_carta_um;
+    float area_carta_um,pib_carta_um,densidade_carta_um,per_capta_carta_um;
 
     //VARIAVEIS CARTA DOIS
     char estado_carta_dois,codigo_carta_dois[4],nome_carta_dois[100];
     int populacao_carta_dois,pontos_turisticos_carta_dois;
-    float area_carta_dois,pib_carta_dois;
+    float area_carta_dois,pib_carta_dois,densidade_carta_dois,per_capta_carta_dois;
 
     //Entrada de dados da carta um;
     printf("DADOS DA CARTA UM \n");
@@ -61,6 +61,12 @@ int main()
 
     printf("NUMERO DE PONTOS TURISTICOS: \n");
     scanf("%i",&pontos_turisticos_carta_dois); 
+
+    //Aqui é feito o calculo da DENSIDADE POPULACIONAL e do PIB PER CAPTA de ambas as cartas.
+    densidade_carta_um  = (float) populacao_carta_um / area_carta_um; 
+    per_capta_carta_um = (float) pib_carta_um / populacao_carta_um;
+    densidade_carta_dois  = (float) populacao_carta_dois / area_carta_dois; 
+    per_capta_carta_dois = (float) pib_carta_dois / populacao_carta_dois;
     
     //SAIDA DOS DADOS DE AMBAS AS CARTAS
 
@@ -75,14 +81,18 @@ int main()
            "Populacao: %d\n"
            "Area: %.2f km2\n"
            "PIB: %.2f bilhoes\n"
-           "Pontos turisticos: %d\n",
+           "Pontos turisticos: %d\n"
+           "Densidade Populacional: %.2f hab/km²\n"
+           "PIB per Capita: %.2f reais\n",
            estado_carta_um,
            codigo_carta_um,
            nome_carta_um,
            populacao_carta_um,
            area_carta_um,
            pib_carta_um,
-           pontos_turisticos_carta_um);
+           pontos_turisticos_carta_um,
+           densidade_carta_um,
+           per_capta_carta_um);
 
     printf("\n--- Informacoes da Carta 02 ---\n"
            "Estado: %c\n"
@@ -91,14 +101,18 @@ int main()
            "Populacao: %d\n"
            "Area: %.2f km2\n"
            "PIB: %.2f bilhoes\n"
-           "Pontos turisticos: %d\n",
+           "Pontos turisticos: %d\n"
+           "Densidade Populacional: %.2f hab/km²\n"
+           "PIB per Capita: %.2f reais\n",
             estado_carta_dois,
             codigo_carta_dois,
             nome_carta_dois,
             populacao_carta_dois,
             area_carta_dois,
             pib_carta_dois,
-            pontos_turisticos_carta_dois);
+            pontos_turisticos_carta_dois,
+            densidade_carta_dois,
+            per_capta_carta_dois);
     
     return 0;
 }
